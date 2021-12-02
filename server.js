4666b030a3bbfb87ca3a3a299ef3f53cbf6a2595
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-12-02 15:24:19 -0600

    server file configuration, variable declarations

--- server.js
+++ server.js
@@ -3,18 +3,35 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3001;
+const helpers = require('./utils/helpers')
 const exphbs =require('express-handlebars');
 require("dotenv").config();
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
 
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 
 
-
-const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
+app.use(routes);
+app.engine('handlebars', hbs.engine);
+app.set('view engine','handlebars');
 
 
 
